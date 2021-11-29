bb8ffedbcd57198e4881f0517e1bc48dc31b55e2
Will <wts012201@gmail.com>
2021-11-28 20:34:51 -0600

    meal plan

--- app/src/main/java/com/example/fatapp/MainActivity.java
+++ app/src/main/java/com/example/fatapp/MainActivity.java
@@ -1,8 +1,17 @@
+
 package com.example.fatapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     Calendar calendar = new Calendar(this);
