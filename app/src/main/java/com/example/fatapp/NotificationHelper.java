e9bd126ea8815005cd4ac623aa3ac7ede3e86689
Davis Shriver <19dshriver@gmail.com>
2021-11-29 13:14:26 -0600

    Update NotificationHelper.java

--- app/src/main/java/com/example/fatapp/NotificationHelper.java
+++ app/src/main/java/com/example/fatapp/NotificationHelper.java
@@ -1,60 +1,40 @@
-package com.firstapp.notifications;
-
-import android.app.Notification;
+import android.app.Application;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.content.Context;
-import android.content.ContextWrapper;
 import android.os.Build;
 
-import androidx.annotation.RequiresApi;
-import androidx.core.app.NotificationCompat;
+public class NotificationHelper extends Application {
 
-public class NotificationHelper extends ContextWrapper {
-    public static final String channel1ID = "channel1ID";
-    public static final String channel1Name = "Channel 1";
-    public static final String channel2ID = "channel2ID";
-    public static final String channel2Name = "Channel 2";
+    public static final String CHANNEL_1_ID = "channel1";
+    public static final String CHANNEL_2_ID = "channel2";
 
-    private NotificationManager mManager;
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public NotificationHelper(Context base) {
-        super(base);
+        createNotificationChannels();
+    }
+
+    private void createNotificationChannels()   {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            createChannels();
+            NotificationChannel channel1 = new NotificationChannel(
+                    CHANNEL_1_ID,
+                    "Channel 1",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel1.setDescription("This is Channel 1");
+
+            NotificationChannel channel2 = new NotificationChannel(
+                    CHANNEL_2_ID,
+                    "Channel 2",
+                    NotificationManager.IMPORTANCE_LOW
+            );
+            channel1.setDescription("This is Channel 2");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel1);
+            manager.createNotificationChannel(channel2);
+
         }
-        createChannels();
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public void createChannels()    {
-        NotificationChannel channel1 = new NotificationChannel(channel1ID, channel1Name, NotificationManager.IMPORTANCE_DEFAULT);
-        channel1.enableLights(true);
-        channel1.enableVibration(true);
-        channel1.setLightColor(R.color.design_default_color_primary);
-        channel1.setLockscreenVisibility((Notification.VISIBILITY_PRIVATE));
-
-        getManager().createNotificationChannel(channel1);
-
-        NotificationChannel channel2 = new NotificationChannel(channel2ID, channel2Name, NotificationManager.IMPORTANCE_DEFAULT);
-        channel2.enableLights(true);
-        channel2.enableVibration(true);
-        channel2.setLightColor(R.color.design_default_color_primary);
-        channel2.setLockscreenVisibility((Notification.VISIBILITY_PRIVATE));
-
-        getManager().createNotificationChannel(channel2);
-    }
-
-    public NotificationManager getManager() {
-        if(mManager == null)    {
-            mManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        }
-
-        return mManager;
-    }
-
-    public NotificationCompat.Builder getChannel1Notification(String title, String message) {
-        return new NotificationCompat.Builder();
     }
 }
