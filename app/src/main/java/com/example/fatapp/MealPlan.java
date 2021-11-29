2f5ecd9af5464878d50d16d240e8a951297e8177
RaulAdhikari <raul.adhikari@mavs.uta.edu>
2021-11-28 20:47:16 -0600

    test

--- app/src/main/java/com/example/fatapp/MealPlan.java
+++ app/src/main/java/com/example/fatapp/MealPlan.java
@@ -14,7 +14,7 @@ import com.google.android.material.navigation.NavigationView;
 
 public class MealPlan extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener { //, CreateMeals.FragmentAListener, TotalCalories.FragmentBListener {
     private DrawerLayout drawer;
-
+//hello
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
