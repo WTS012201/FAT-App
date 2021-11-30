0448f68895403b6ac2ad78873f6a8849239a97d6
Will <wts012201@gmail.com>
2021-11-30 15:16:32 -0600

    'mealplan_remove'

--- app/src/main/java/com/example/fatapp/ExampleAdapter.java
+++ app/src/main/java/com/example/fatapp/ExampleAdapter.java
@@ -3,6 +3,8 @@ package com.example.fatapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,7 +23,6 @@ public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleV
             super(itemView);
             mTextViewLine1 = itemView.findViewById(R.id.textview_line1);
             mTextViewLine2 = itemView.findViewById(R.id.textview_line_2);
-
         }
     }
 
@@ -33,14 +34,23 @@ public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleV
     public ExampleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.example_items, parent, false);
         ExampleViewHolder evh = new ExampleViewHolder(v);
+        Button remove = v.findViewById(R.id.remove);
+        remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println(evh.getAdapterPosition());
+                mExampleList.remove(evh.getAdapterPosition());
+                ExampleAdapter.super.notifyItemRemoved(evh.getAdapterPosition());
+            }
+        });
         return evh;
     }
-
     @Override
     public void onBindViewHolder(ExampleViewHolder holder, int position) {
         ExampleItem currentItem = mExampleList.get(position);
         holder.mTextViewLine1.setText(currentItem.getLine1());
         holder.mTextViewLine2.setText(currentItem.getLine2());
+
     }
 
     @Override
